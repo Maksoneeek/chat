1c64df6bd202af8d9d31f9b8a068464f1c1a45f0
Makxim Ismagilov <maksoneeeek@mail.ru>
2020-10-23 07:16:23 +0700

    templates

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
     $('.new-chat').click(function (event) {
         $('.popup-new').toggleClass('open');    
     });
+    $('.templates-open').click(function (event) {
+        $('.templates').toggleClass('open');    
+    });
     $('.papki').click(function (event) {
         $('.chat').toggleClass('pad'); 
         $('.letters-chat__text,.letters-chat__new-chat').toggleClass('visible');    
