739c4ad17fc3e595c84c0245843a7ee5c8790ccc
Makxim Ismagilov <maksoneeeek@mail.ru>
2020-10-24 08:49:13 +0700

    settings

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ $(document).ready(function () {
     $('.templates-open').click(function (event) {
         $('.templates').toggleClass('open');    
     });
+    $('.settings-open').click(function (event) {
+        $('.settings').toggleClass('open');    
+    });
+    $('.new-templates-open').click(function (event) {
+        $('.new-templates').toggleClass('open'); 
+        $('.settings').removeClass('open');  
+    });
+    $('.new-templates__back').click(function (event) {
+        $('.new-templates').removeClass('open');
+        $('.settings').addClass('open');
+    });
     $('.papki').click(function (event) {
         $('.chat').toggleClass('pad'); 
         $('.letters-chat__text,.letters-chat__new-chat').toggleClass('visible');    
@@ -15,4 +26,19 @@ $(document).ready(function () {
         $('.letters-chat__new-chat-text').toggleClass('none');    
     });
 });
+$(document).ready(function () {
+    $('.__select__title').click(function (event) {
+        const select = $(this).parents('.__select').first()
+        if (!select.attr('data-state')) {
+            select.attr('data-state', 'active')
+        } else {
+            select.attr('data-state', '')
+        }
 
+    });
+    $('.__select__label').click(function (event) {
+        console.log($(this).closest('.__select').children('.__select__title').text(event.target.textContent))
+        $(this).parents('.__select').first().attr('data-state', '')
+    });
+
+});
