a6ab4448d05925ec04dcb1e0d4b5b9ad5021b6c4
Makxim Ismagilov <maksoneeeek@mail.ru>
2020-10-25 15:50:09 +0700

    checkbox+fix settings

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 
 $(document).ready(function () {
     $('.new-chat').click(function (event) {
-        $('.popup-new').toggleClass('open');    
+        $('.popup-new').toggleClass('open');
     });
     $('.templates-open').click(function (event) {
-        $('.templates').toggleClass('open');    
+        $('.templates').toggleClass('open');
     });
     $('.settings-open').click(function (event) {
-        $('.settings').toggleClass('open');    
+        $('.settings').toggleClass('open');
     });
     $('.new-templates-open').click(function (event) {
-        $('.new-templates').toggleClass('open'); 
-        $('.settings').removeClass('open');  
+        $('.new-templates').toggleClass('open');
+        $('.settings').removeClass('open');
     });
     $('.new-templates__back').click(function (event) {
         $('.new-templates').removeClass('open');
         $('.settings').addClass('open');
     });
     $('.papki').click(function (event) {
-        $('.chat').toggleClass('pad'); 
-        $('.letters-chat__text,.letters-chat__new-chat').toggleClass('visible');    
+        $('.chat').toggleClass('pad');
+        $('.letters-chat__text,.letters-chat__new-chat').toggleClass('visible');
     });
     $('.letters-chat__new-chat-text').click(function (event) {
-        $('.letters-chat__form').toggleClass('open'); 
-        $('.letters-chat__new-chat-text').toggleClass('none');    
+        $('.letters-chat__form').toggleClass('open');
+        $('.letters-chat__new-chat-text').toggleClass('none');
     });
 });
 $(document).ready(function () {
@@ -40,5 +40,20 @@ $(document).ready(function () {
         console.log($(this).closest('.__select').children('.__select__title').text(event.target.textContent))
         $(this).parents('.__select').first().attr('data-state', '')
     });
-
+});
+$(document).ready(function () {
+    $.each($('.checkbox'), function (index, val) {
+        if ($(this).find('input').prop('checked') == true) {
+            $(this).addClass('active');
+        }
+    });
+    $(document).on('click', '.checkbox', function (event) {
+        if ($(this).hasClass('active')) {
+            $(this).find('input').prop('checked', false);
+        } else {
+            $(this).find('input').prop('checked', true);
+        }
+        $(this).toggleClass('active')
+        return false;
+    });
 });
