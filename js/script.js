07aeb0406507a9018eb4fb8d27fc1505f42092e5
Makxim Ismagilov <maksoneeeek@mail.ru>
2020-11-04 10:28:01 +0700

    textarea

--- js/script.js
+++ js/script.js
@@ -94,24 +94,3 @@ $(document).ready(function () {
     });
 });
 
-function setTimer() {
-    let time = 0
-    const timerInput = document.querySelector('.micro__timer')
-    timerInput.style.fontSize = 18 + 'px'
-    const timer = setInterval(function () {
-        time = time + 1
-        let mins = Math.floor(time / 60)
-        let seconds = time % 60
-        if (seconds < 10) {
-            seconds = '0' + seconds
-        }
-        timerInput.textContent = `${mins}:${seconds}`
-        if (time == 0) {
-            clearInterval(timer)
-            timerInput.style.fontSize = 0
-            document.querySelector('.popup__restart').style.fontSize = 12 + 'px'
-            document.querySelector('.popup__restart').addEventListener('click', restartTimer)
-        }
-    }, 1000)
-    alert('End!');
-}
